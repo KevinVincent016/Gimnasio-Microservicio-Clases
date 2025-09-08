08d3bab52d78d8ab863105693f360ebdf61c98b5
JuanDi <juandiroca@hotmail.com>
2025-09-08 09:38:53 -0500

    “Dejé montada la base de RabbitMQ: compose en la raíz y config común en los 4 servicios (exchange, cola y bindings). Activé @EnableRabbit y las props; al arrancar se crean gimnasio.exchange y clases.events. Falta el publisher y los listeners.”

--- src/main/java/co/analisys/gimnasio/ClasesApplication.java
+++ src/main/java/co/analisys/gimnasio/ClasesApplication.java
@@ -3,9 +3,14 @@ package co.analisys.gimnasio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
+
+@EnableRabbit
 @SpringBootApplication
 @EnableFeignClients
 public class ClasesApplication {
