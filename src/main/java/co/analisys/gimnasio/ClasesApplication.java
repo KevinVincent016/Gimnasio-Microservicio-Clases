9bff651f525d3a7dc0b724c5ee3efc1ed67bb751
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2025-10-05 10:18:55 -0500

    chore: add eureka client dependency

--- src/main/java/co/analisys/gimnasio/ClasesApplication.java
+++ src/main/java/co/analisys/gimnasio/ClasesApplication.java
@@ -2,6 +2,7 @@ package co.analisys.gimnasio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableRabbit
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ClasesApplication {
 
     public static void main(String[] args) {
